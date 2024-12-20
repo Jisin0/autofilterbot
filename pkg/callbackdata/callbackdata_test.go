1e8033a208d82fb4e31fbcbfefc35609c05c422a
josejisin@gmail.com <josejisin@gmail.com>
2024-12-20 23:09:08 +0000

    test: callbackdata basic tests

---
+++ pkg/callbackdata/callbackdata_test.go
@@ -0,0 +1,25 @@
+package callbackdata_test
+
+import (
+	"testing"
+
+	"github.com/Jisin0/autofilterbot/pkg/callbackdata"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCallbackData(t *testing.T) {
+	assert := assert.New(t)
+
+	{
+		c := callbackdata.New().AddPath("dayumm").AddArg("foo")
+
+		assert.Equal("dayumm|foo", c.ToString())
+	}
+
+	{
+		c := callbackdata.FromString("paf1:paf2|arg1_arg2")
+
+		assert.Equal([]string{"paf1", "paf2"}, c.Path)
+		assert.Equal([]string{"arg1", "arg2"}, c.Args)
+	}
+}
