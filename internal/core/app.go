97ed701f86040fd7169b5100bc2e57c76906dc74
josejisin@gmail.com <josejisin@gmail.com>
2025-01-18 21:41:51 +0000

    perf: unnecessary cnfig map conversion

--- internal/core/app.go
+++ internal/core/app.go
@@ -186,7 +186,6 @@ func (app *Core) RefreshConfig() {
 		app.Log.Error("failed to refresh configs", zap.Error(err))
 	}
 	app.Config = c
-	app.Config.RefreshMap()
 }
 
 // App returns the initialized global app instance.
